081ae61958942934b58b6f8adc1d57e9c1a40e3c
Vangel22 <h.vangel22@gmail.com>
2023-08-22 20:47:31 +0200

    switch

--- c1/calculator.js
+++ c1/calculator.js
@@ -1,8 +1,23 @@
 // mnozenje, delenje, sobiranje, odzimanje
 
 //operacija, parametar 1, parametar 2
-function calculator() {
-  // switch ili if blokovi
+function calculator(operacija, param1, param2) {
+  let res = 0;
+  switch (operacija) {
+    case "sobiranje":
+      //   res = param1 + param2;
+      //   break;
+      return param1 + param2;
+    case "odzemanje":
+      return param1 - param2;
+    case "mnozenje":
+      return param1 * param2;
+    case "delenje":
+      return param1 / param2;
+    default:
+      return "Not recognized operation";
+  }
+  //   return res;
 }
 
-// calculator('mnozenje', 2,2)
+console.log(calculator("mnozenje", 2, 2));
