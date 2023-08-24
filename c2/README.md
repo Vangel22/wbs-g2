d3be72ebfc76ebbdf5bdbdfd292b52abd710b8e2
Vangel22 <h.vangel22@gmail.com>
2023-08-24 20:24:32 +0200

    Save class 2

---
+++ c2/README.md
@@ -0,0 +1,17 @@
+- REST (Representational State Transfer) - arhitekturni principi i granici sto ni davaat
+  standardiziran nacin na dizajniranje na aplikacii na internet
+- Design pattern - sledi nekoi granici, i toa na koj nacin ke se organizira kodot.
+
+- RESTFUL API - ovozmozuva komunikacija na softverski komponenti
+- Web servisi - komunikacija na klient/server preku internet
+
+- folder1
+- folder2
+- folder3
+- folder4
+
+- REST
+  - koristi klient-server arhitektura
+  - statlessness - znaci ne skladira baranja od klientot tuku samo gi opsluzuva
+  - kesiranje -
+    - pr. Klient pobaruva resurs Smetkovodstvo 2021
---
+++ c2/index.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+
+// GET, POST, PUT, PATCH, DELETE
+
+// read file
+const readData = (source) => {
+  return new Promise((success, fail) => {
+    fs.readFile(`${source}.json`, "utf-8", (err, data) => {
+      if (err) return fail(err);
+      const out = JSON.parse(data);
+      return success(out);
+    });
+  });
+};
+// write file
+const writeData = (data, destination) => {
+  return new Promise((success, fail) => {
+    const out = JSON.stringify(data);
+    fs.writeFile(`${destination}.json`, out, (err) => {
+      if (err) return fail(err);
+      return success();
+    });
+  });
+};
+
+// Create
+
+const addPerson = async (id, firstname, lastname) => {
+  try {
+    const person = {
+      id: id,
+      firstname: firstname,
+      lastname: lastname,
+    };
+    let data = await readData("./data");
+    data.push(person);
+    await writeData(data, "./data");
+  } catch (err) {
+    throw err;
+  }
+};
+
+//IIFE
+(async function () {
+  await addPerson(4, "Semos", "Education");
+  await addPerson(5, "Petar", "EdenDva");
+})();
+
+// Remove
+
+// Update
+
+// JSON.parse - od JSON vo JS
+// JSON.stringify - od objekt vo JSON
