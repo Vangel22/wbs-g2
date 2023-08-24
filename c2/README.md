b7da363b2ad5a99fa922a4db70a5dad315f197da
Vangel22 <h.vangel22@gmail.com>
2023-08-24 22:03:28 +0200

    Save class 2

--- c2/README.md
+++ c2/README.md
@@ -1,9 +1,12 @@
 - REST (Representational State Transfer) - arhitekturni principi i granici sto ni davaat
   standardiziran nacin na dizajniranje na aplikacii na internet
-- Design pattern - sledi nekoi granici, i toa na koj nacin ke se organizira kodot.
+- Design pattern - sledi nekoi orgranicuvanja, i toa na koj nacin ke se organizira i koristi kodot.
 
 - RESTFUL API - ovozmozuva komunikacija na softverski komponenti
 - Web servisi - komunikacija na klient/server preku internet
+- API e kako dogovor kade sto e definirano kakva interakcija ke imaat softverskite komponenti
+  pomegju sebe, dodeka veb servisite se specificna implementacija na API koja ovozmozuva komunikacija
+  preku internet ili mreza.
 
 - folder1
 - folder2
@@ -13,5 +16,5 @@
 - REST
   - koristi klient-server arhitektura
   - statlessness - znaci ne skladira baranja od klientot tuku samo gi opsluzuva
-  - kesiranje -
+  - kesiranje
     - pr. Klient pobaruva resurs Smetkovodstvo 2021
--- c2/index.js
+++ c2/index.js
@@ -40,6 +40,58 @@ const addPerson = async (id, firstname, lastname) => {
   }
 };
 
+// Update
+
+// Spread
+// const obj1 = {
+//   name: "One",
+//   age: 20,
+// };
+
+// const obj2 = {
+//   name: "Two",
+//   age: 22,
+//   country: "Macedonia",
+// };
+
+// const obj3 = {
+//   ...obj1,
+// name: "One",
+// age: 20
+//   ...obj2,
+// name: "Two",
+// age: 22
+// country: "Macedonia"
+// };
+
+// obj3.name = "One"
+// obj3.age = 20
+
+// console.log("obj3", obj3);
+
+const updatePerson = async (id, newPersonData) => {
+  try {
+    let data = await readData("./data");
+    const person = data.find((user) => user.id == id);
+    //{"id":1,"first_name":"Pero","last_name":"Perovski"}
+    const newPerson = {
+      ...person,
+      // id: 1
+      // first_name: "Pero"
+      // last_name: "Perovski"
+      ...newPersonData,
+      // first_name: "PeroDva"
+    };
+    data = data.filter((user) => user.id !== id); // bez covekot koj probuvame da go azurirame
+    data.push(newPerson);
+    await writeData(data, "./data");
+  } catch (err) {
+    throw err;
+  }
+};
+
+// Remove
+
 const removePerson = async (id) => {
   try {
     const data = await readData("./data");
@@ -51,10 +103,47 @@ const removePerson = async (id) => {
 };
 
 //IIFE
+// (async function () {
+//   await updatePerson(1, {
+//     first_name: "PeroDva",
+//   });
+// })();
+
+const updateCar = async (id, newCarData) => {
+  try {
+    let data = await readData("./cars");
+    const car = data.find((car) => car.id === id);
+    const newCar = {
+      ...car,
+      //     "id": 1,
+      //     "brand": "Toyota",
+      //     "model": "Camry",
+      //     "year": 2022
+      ...newCarData,
+      //     "brand": "BMW",
+    };
+    data = data.filter((car) => car.id !== id); // -1 car
+    // go prebrisuvame momentalniot avtomobil za da
+    //go dodame noviot so novite podatoci
+    data.push(newCar); // +1 car
+    await writeData(data, "./cars");
+  } catch (err) {
+    throw err;
+  }
+};
+
+const removeCar = async (id) => {
+  try {
+    const data = await readData("./cars");
+    const cars = data.filter((car) => car.id !== id);
+    await writeData(cars, "./cars");
+  } catch (err) {
+    throw err;
+  }
+};
+
 (async function () {
-  await addPerson(4, "Semos", "Education");
-  await addPerson(5, "Petar", "EdenDva");
-  await removePerson(4);
+  await removeCar(1);
 })();
 
 // Napisete go ova so obicna funkcija, namesto vo IIFE
@@ -65,3 +154,8 @@ const removePerson = async (id) => {
 
 // JSON.parse - od JSON vo JS
 // JSON.stringify - od objekt vo JSON
+
+// Homework
+// Define 5 different REST Resources - JSON -
+// user.json, books.json, cars.json, locations.json, products.json
+// CRUD for ONE of the models (use readData and writeData)
